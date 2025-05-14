0f8d61abd2dfda34377c3bee41536d126d78a465
Md Munna Mia <mdmunnamia987@gmail.com>
2025-05-14 20:40:44 +0600

    add basic applicaiton configuration

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type Application struct{}
+
+func main() {
+	server := http.Server{
+		Addr: "localhost:8080",
+	}
+
+	log.Printf("Server is running at PORT : %s", server.Addr)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
