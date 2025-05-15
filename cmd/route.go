280fe1d0e8ce156fa0d18b4c51461d7256e44f6e
Md Munna Mia <mdmunnamia987@gmail.com>
2025-05-15 17:54:00 +0600

    add application routes

--- cmd/route.go
+++ cmd/route.go
@@ -1,1 +1,14 @@
 package main
+
+import "net/http"
+
+func (app *Application) route() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/form", app.form)
+	mux.HandleFunc("/list", app.list)
+	mux.HandleFunc("/about", app.about)
+
+	return mux
+}
