0394488c180f31697a035305bb291eb777420159
Md Munna Mia <mdmunnamia987@gmail.com>
2025-05-15 17:53:36 +0600

    add application handlers methods

--- cmd/handler.go
+++ cmd/handler.go
@@ -1,1 +1,8 @@
 package main
+
+import "net/http"
+
+func (app *Application) home(w http.ResponseWriter, r *http.Request)  {}
+func (app *Application) form(w http.ResponseWriter, r *http.Request)  {}
+func (app *Application) list(w http.ResponseWriter, r *http.Request)  {}
+func (app *Application) about(w http.ResponseWriter, r *http.Request) {}
