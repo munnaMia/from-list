ee8c3a94dc9a1f5bd21ea71f139d51aa63726609
Md Munna Mia <mdmunnamia987@gmail.com>
2025-05-15 17:52:55 +0600

    add app.route method to the application

--- cmd/main.go
+++ cmd/main.go
@@ -8,8 +8,11 @@ import (
 type Application struct{}
 
 func main() {
+	app := &Application{}
+
 	server := http.Server{
-		Addr: "localhost:8080",
+		Addr:    "localhost:8080",
+		Handler: app.route(),
 	}
 
 	log.Printf("Server is running at PORT : %s", server.Addr)
