ba45fbc9608bedc70609601b61b313db0f297284
Md Munna Mia <mdmunnamia987@gmail.com>
2025-05-17 12:23:46 +0600

    update home router with basic template

--- cmd/handler.go
+++ cmd/handler.go
@@ -1,8 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
-func (app *Application) home(w http.ResponseWriter, r *http.Request)  {}
+func (app *Application) home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.Error(w, "Page Not Found", http.StatusNotFound)
+		return
+	}
+	
+	tmpl := template.Must(template.ParseFiles("./ui/html/home.html"))
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		panic(err)
+	}
+
+}
 func (app *Application) form(w http.ResponseWriter, r *http.Request)  {}
 func (app *Application) list(w http.ResponseWriter, r *http.Request)  {}
 func (app *Application) about(w http.ResponseWriter, r *http.Request) {}
