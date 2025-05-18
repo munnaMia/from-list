441561c47a126b23c1774cafdc0f64eaca8d51b0
Md Munna Mia <mdmunnamia987@gmail.com>
2025-05-18 10:55:05 +0600

    update on form, list, about handlers

--- cmd/handler.go
+++ cmd/handler.go
@@ -24,6 +24,39 @@ func (app *Application) home(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
-func (app *Application) form(w http.ResponseWriter, r *http.Request)  {}
-func (app *Application) list(w http.ResponseWriter, r *http.Request)  {}
-func (app *Application) about(w http.ResponseWriter, r *http.Request) {}
+func (app *Application) form(w http.ResponseWriter, r *http.Request)  {
+	// Page render data.
+	data := map[string]any{
+		"Title": "Form Page",
+	}
+
+	tmpl := template.Must(template.ParseFiles("./ui/html/layout/base.html", "./ui/html/partials/header.html", "./ui/html/partials/footer.html", "./ui/html/partials/nab.html", "./ui/html/pages/form.html"))
+
+	if err := tmpl.Execute(w, data); err != nil {
+		panic(err)
+	}
+}
+func (app *Application) list(w http.ResponseWriter, r *http.Request)  {
+	// Page render data.
+	data := map[string]any{
+		"Title": "List Page",
+	}
+
+	tmpl := template.Must(template.ParseFiles("./ui/html/layout/base.html", "./ui/html/partials/header.html", "./ui/html/partials/footer.html", "./ui/html/partials/nab.html", "./ui/html/pages/list.html"))
+
+	if err := tmpl.Execute(w, data); err != nil {
+		panic(err)
+	}
+}
+func (app *Application) about(w http.ResponseWriter, r *http.Request) {
+	// Page render data.
+	data := map[string]any{
+		"Title": "About Page",
+	}
+
+	tmpl := template.Must(template.ParseFiles("./ui/html/layout/base.html", "./ui/html/partials/header.html", "./ui/html/partials/footer.html", "./ui/html/partials/nab.html", "./ui/html/pages/about.html"))
+
+	if err := tmpl.Execute(w, data); err != nil {
+		panic(err)
+	}
+}
