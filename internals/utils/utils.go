c16b4a793c7eb17ae28439d3d1596ebff3994e60
Md Munna Mia <mdmunnamia987@gmail.com>
2025-05-20 19:58:21 +0600

    update Check method & add CreateIfNotExist method

--- internals/utils/utils.go
+++ internals/utils/utils.go
@@ -1,7 +1,42 @@
 package utils
 
-import "log"
+import (
+	"log"
+	"os"
+	"path/filepath"
+)
 
 func Check(err error) {
-	log.Fatal(err)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Create file and directory if not exist.
+func CreateIfNotExist(path string) {
+	absFilePath, err := filepath.Abs(path)
+	Check(err)
+
+	// Creating a directory if not exist
+	dir := filepath.Dir(absFilePath)
+
+	_, err = os.Stat(dir)
+	Check(err)
+
+	if os.IsNotExist(err) {
+		err := os.MkdirAll(dir, 0755)
+		Check(err)
+	}
+
+	//create file if not exist
+	_, err = os.Stat(absFilePath)
+
+	if os.IsNotExist(err) {
+		file, err := os.Create(absFilePath)
+		Check(err)
+		defer file.Close()
+		_, err = file.Write([]byte("[]"))
+		Check(err)
+	}
+
 }
