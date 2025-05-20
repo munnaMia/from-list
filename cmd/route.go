9fb5a1aaa62b3d1f331bb167bbfbf628ffda6d88
Md Munna Mia <mdmunnamia987@gmail.com>
2025-05-20 10:20:21 +0600

    add forminput route

--- cmd/route.go
+++ cmd/route.go
@@ -8,6 +8,7 @@ func (app *Application) route() *http.ServeMux {
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./ui/static/"))))
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/form", app.form)
+	mux.HandleFunc("/forminput", app.fromInput)
 	mux.HandleFunc("/list", app.list)
 	mux.HandleFunc("/about", app.about)
 
